441938ae543aa7309f146272b76daefdcc8cb293
Raul Rodriguez <raulrobi@gmail.com>
2020-12-09 16:01:08 -0300

    Class 69 - using Partials with HBS

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const app = express();
 
+const hbs = require("hbs");
+
 //Here is using a middlewere that always is executed
 //In this case calls the index.html in the public folder
 app.use(express.static(__dirname + "/public"));
+
+hbs.registerPartials(__dirname + "/views/partials", (err) => {
+    console.log(err);
+});
 
 // Express with hbs
 app.set("view engine", "hbs");
 
 app.get("/", function (req, res) {
     //The second param permite sen data to the page.
-    res.render("home", {
+    res.render("index", {
         name: "Raul Rodriguez",
         year: new Date().getFullYear()
     });
