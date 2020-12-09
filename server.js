97e6a182d6b1ef6e651e6e4264817bda93da42bc
Raul Rodriguez <raulrobi@gmail.com>
2020-12-09 09:30:30 -0300

    Publish html pages with express server

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
+//Here is using a middlewere that always is executed
+//In this case calls the index.html in the public folder
+app.use(express.static(__dirname + "/public"));
+
+/* app.get("/", function (req, res) {
     let data = {
         name: "Raul",
         age: 42,
         url: req.url
     };
     res.send(data);
-});
+}); */
 
-app.get("/data", function (req, res) {
+/* app.get("/data", function (req, res) {
     res.send("Hello data");
-});
+}); */
 
 app.listen(3000, () => {
     console.log("Listening port 3000");
