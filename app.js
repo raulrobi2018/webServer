bde28406bea7abbb221ec70e52309ee296c98ef7
Raul Rodriguez <raulrobi@gmail.com>
2020-12-09 09:11:12 -0300

    Class 64 - Creating a server with http node

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const {RSA_NO_PADDING} = require("constants");
 const http = require("http");
+
+try {
+    http.createServer((req, res) => {
+        res.writeHead(200, {"Content-Type": "application/json"});
+
+        let data = {
+            name: "Raul",
+            age: 42,
+            url: req.url
+        };
+        res.write(JSON.stringify(data));
+        res.end();
+    }).listen(3000);
+
+    console.log("Escuchando puerto 3000");
+} catch (error) {
+    console.log(error);
+}
