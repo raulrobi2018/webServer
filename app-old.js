a60749aa80560a17527b33a953236ba27054fe82
Raul Rodriguez <raulrobi@gmail.com>
2022-08-13 21:19:07 -0300

    Actualización del curso

--- app-old.js
+++ app-old.js
@@ -3,14 +3,24 @@ const http = require("http");
 
 try {
     http.createServer((req, res) => {
-        res.writeHead(200, {"Content-Type": "application/json"});
+        /* res.writeHead(200, {"Content-Type": "application/json"});
 
         let data = {
             name: "Raul",
             age: 42,
             url: req.url
         };
-        res.write(JSON.stringify(data));
+        res.write(JSON.stringify(data));*/
+
+        res.setHeader("Content-Disposition", "attachment; filename=lista.csv");
+        res.writeHead(200, {"Content-Type": "application/csv"});
+
+        res.write("id, nombre\n");
+        res.write("1, Raul\n");
+        res.write("2, Silvia\n");
+        res.write("3, Ana\n");
+        res.write("4, Ceci\n");
+
         res.end();
     }).listen(3000);
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require(".dotenv").config();
 
 const hbs = require("hbs");
 
@@ -8,8 +9,10 @@ const port = process.env.PORT || 3000;
 
 require("./hbs/helpers");
 
+//Serving static content
 //Here is using a middlewere that always is executed
 //In this case calls the index.html in the public folder
+//__dirname: it is the path where our app is installed
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerPartials(__dirname + "/views/partials", (err) => {
@@ -19,14 +22,22 @@ hbs.registerPartials(__dirname + "/views/partials", (err) => {
 // Express with hbs
 app.set("view engine", "hbs");
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     //The second param permits send data to the page.
-    res.render("home");
+    res.render("home", {
+        name: "Raul Rodriguez",
+        title: "Web Server App - Node course"
+    });
 });
 
-app.get("/about", function (req, res) {
+app.get("/about", (req, res) => {
     //The second param permits send data to the page.
     res.render("about");
+});
+
+//Any other route will enter here
+app.get("/*", (req, res) => {
+    res.render("404");
 });
 
 /* app.get("/data", function (req, res) {
